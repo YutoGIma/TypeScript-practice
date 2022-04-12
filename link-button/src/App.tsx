a28def8b122f381eb92ea1df7b79650d4eb5c71d
Yuto GIma <yurihanagima@gmail.com>
2022-04-12 13:16:37 +0000

    子コンポーネント実装

--- link-button/src/App.tsx
+++ link-button/src/App.tsx
@@ -1,11 +1,20 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isPropertySignature } from 'typescript';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ul>
+          {items.map((item: Item) => (
+            <li key = {item.id}>{item.title}</li>
+          ))}
+        </ul>
+        <div className="App">
+          <Child  />
+        </div>
         <LinkButton />
       </header>
     </div>
@@ -20,4 +29,27 @@ function LinkButton() {
   return <span className='likeButton' onClick={handlClick}>💛{count}</span>
 }
 
+type Item = {
+  id: number
+  title: string
+}
+
+const items: Item[] = [
+  {
+    id: 1,
+    title: "ハリーポッターと賢者の石"
+  },
+  {
+    id: 2,
+    title: "ハリーポッターと死の秘宝"
+  }
+]
+
+function Child(){
+  return (
+    <div>
+      <p>これは子コンポーネントです。</p>
+    </div>
+  )
+}
 export default App;
