2648936e78f1ad48f9515f0910af4099b83d1f90
Yuto GIma <yurihanagima@gmail.com>
2022-04-17 13:07:35 +0000

    入力の変更を検知して表示の変更を実装

--- link-button/src/input.tsx
+++ link-button/src/input.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 type Props = {
     children: string
 }
 
 const Input: React.FC<Props> = ({children}) => {
+    const [name, setName] = React.useState(children)
+    const handleChange = (e:any) => {
+        setName(e.target.value)
+    } 
     return (
-        <div className="formInput">
-            <input type="text" defaultValue={ children }/>
-            <p>{ children }</p>
-        </div>
+        <form className="formInput" onSubmit={ clickEnter }>
+            <input name="name" type="text" defaultValue={ name } onChange={ handleChange }/>
+            <p>{ name }</p>
+            <button type="submit">送信</button>
+        </form>
     )
+}
+
+function clickEnter(){
+    console.log("yes")
 }
 
 export default Input
