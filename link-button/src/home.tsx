2b82ef8de7a7ca3a3b22543187206539f1faf630
Yuto GIma <yurihanagima@gmail.com>
2022-06-03 14:03:41 +0000

    シンデザイン

--- link-button/src/home.tsx
+++ link-button/src/home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 const Home: React.FC = () => {
     return(
-        <div id="home">
-            <h1>ReactAPP</h1>
-        </div>
+        <main>
+            <div id="home">
+                <div className="" id="menu">
+                    <p>menu1</p>
+                    <p>menu2</p>
+                    <p>menu3</p>
+                    <p>menu4</p>
+                    <p>menu5</p>
+                </div>
+                <div id="main">
+                    <div className="main-content"></div>
+                </div>
+            </div>
+        </main>
     )
 }
 
